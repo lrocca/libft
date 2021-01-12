534d9507c642570b447e4c1d48e211a513b53916
Leonardo Rocca <lrocca@student.42roma.it>
2021-01-12 12:19:32 +0100

    Now atoi works as expected. Added support for other types of spacing characters. Simplyfied sign determination. Corrected main logic.

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: lrocca <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/11 15:37:07 by lrocca            #+#    #+#             */
-/*   Updated: 2021/01/11 17:35:52 by lrocca           ###   ########.fr       */
+/*   Updated: 2021/01/12 10:08:40 by lrocca           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,17 +19,17 @@ int	ft_atoi(const char *str)
 
 	n = 0;
 	sign = 1;
-	while (*str == ' ')
+	while (*str == 32 || (*str >= 9 && *str <= 13))
 		str++;
 	if (*str == '+' || *str == '-')
 	{
 		if (*str == '-')
-			sign *= -1;
+			sign = -1;
 		str++;
 	}
-	while (*str >= '0' && *str <= '9')
+	while (*str && (*str >= '0' && *str <= '9'))
 	{
-		n += n * 10 + *str - '0';
+		n = n * 10 + *str - '0';
 		str++;
 	}
 	return (sign * n);
