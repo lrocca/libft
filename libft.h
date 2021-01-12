88ba1197179c87ea8152772c9bebb43b185f44d8
Leonardo Rocca <lrocca@student.42roma.it>
2021-01-12 12:26:21 +0100

    3 ft_mem and bzero work as expected.

--- libft.h
+++ libft.h
@@ -15,6 +15,10 @@
 
 # include <string.h>
 
+void	*ft_memset(void *b, int c, size_t len);
+void	ft_bzero(void *s, size_t n);
+void	*ft_memcpy(void *dst, const void *src, size_t n);
+void	*ft_memccpy(void *dst, const void *src, int c, size_t n);
 size_t	ft_strlen(const char *s);
 char	*ft_strchr(const char *s, int c);
 int		ft_atoi(const char *str);
