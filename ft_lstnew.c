fe76ae57de18500ea4f07d87b17a676c9965f56a
Leonardo Rocca <lrocca@student.42roma.it>
2021-01-18 11:34:25 +0100

    Fixed lstnew (initialized next to NULL)

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -6,7 +6,7 @@
 /*   By: lrocca <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/15 22:32:44 by lrocca            #+#    #+#             */
-/*   Updated: 2021/01/15 22:42:25 by lrocca           ###   ########.fr       */
+/*   Updated: 2021/01/18 11:32:31 by lrocca           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,6 @@ t_list	*ft_lstnew(void *content)
 	if (!(new = malloc(sizeof(t_list))))
 		return (NULL);
 	new->content = content;
+	new->next = NULL;
 	return (new);
 }
