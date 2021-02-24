133ddc5b82b5f15fdb7f0d70c0bac8939cba0557
Leonardo Rocca <lrocca@student.42roma.it>
2021-02-24 19:58:19 +0100

    Added ft_isspace

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: lrocca <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/11 14:47:55 by lrocca            #+#    #+#             */
-/*   Updated: 2021/02/03 16:01:19 by lrocca           ###   ########.fr       */
+/*   Updated: 2021/02/23 18:40:05 by lrocca           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@
 
 int				ft_isupper(int c);
 int				ft_islower(int c);
+int				ft_isspace(int c);
 size_t			ft_numlen(unsigned long long n, int base);
 char			*ft_utoa_base(unsigned long long n, char *radix);
 
