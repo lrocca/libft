441bb15d9236e30d0a8fbfa5d9d7ca2588562ee3
Leonardo Rocca <lrocca@student.42roma.it>
2021-05-10 17:21:18 +0200

    fix and refactor `ft_itoa`

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: lrocca <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/13 11:51:30 by lrocca            #+#    #+#             */
-/*   Updated: 2021/05/10 16:18:43 by lrocca           ###   ########.fr       */
+/*   Updated: 2021/05/10 17:16:44 by lrocca           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,29 +14,26 @@
 
 char	*ft_itoa(int n)
 {
+	char	*s;
 	size_t	len;
-	char	*str;
 	int		sign;
 
-	sign = 0;
-	len = 0;
-	if (n < 0)
-	{
-		n *= -1;
-		sign = 1;
-		len++;
-	}
-	len += ft_numlen((unsigned int)n, 10);
-	str = malloc(len + 1);
-	if (!str)
-		return (NULL);
-	str[len--] = '\0';
+	if (n == -2147483648)
+		return (ft_strdup("-2147483648"));
+	sign = n < 0;
 	if (sign)
-		str[0] = '-';
+		n *= -1;
+	len = ft_numlen((unsigned int)n, 10) + sign;
+	s = malloc(len + 1);
+	if (!s)
+		return (NULL);
+	s[len--] = '\0';
+	if (sign)
+		s[0] = '-';
 	while (len + 1 > (size_t)sign)
 	{
-		str[len--] = '0' + (n % 10);
+		s[len--] = '0' + (n % 10);
 		n /= 10;
 	}
-	return (str);
+	return (s);
 }
